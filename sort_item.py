23125484578889f1c10658ebf1c3f2b35eddc877
Md.Rashedul Amin Rony <md.rashedul.amin@gmail.com>
2021-12-19 04:01:38 +0600

    Reelase v0.0.1: Updated comments on code for better understanding

--- sort_item.py
+++ sort_item.py
@@ -6,7 +6,9 @@ def sorting(list_of_tuples):
     print(f'Sorted Inputs: {list_of_tuples}')
 
 if __name__ == "__main__":
+    # Get how much inputs will be provided from command line
     item_no = input('Set the total number of tuples to insert in integer: ')
+    # Handle if wrong input type provided
     try:
         item_no = int(item_no)
     except Exception as err:
@@ -17,8 +19,12 @@ if __name__ == "__main__":
     for i in range(item_no):
         item = input('Add comma seperated Name(String),Age (Int),Score (Int): ')
         item = item.split(',')
+
+        # Handle if more items provided rather Name,Age,Score
         if(len(item)>3):
             sys.exit('More than 3 items provided')
+
+        # Convert to accurate types
         try:
             item = (str(item[0]),int(item[1]),int(item[2]))
         except Exception as err:
