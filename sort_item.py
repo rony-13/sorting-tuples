17b5ed2ca9a7c7a423c4f5ca873d57b7835c6f0d
Md.Rashedul Amin Rony <md.rashedul.amin@gmail.com>
2021-12-19 03:49:23 +0600

    Release v0.0.1: Updated to make it more modular and compatible for importing and used by other modules

--- sort_item.py
+++ sort_item.py
@@ -1,28 +1,28 @@
 import sys
-item_no = input('Set the total number of tuples to insert in integer: ')
 
 def sorting(list_of_tuples):
+    print(f'Unsorted Inputs: {list_of_tuples}')
     list_of_tuples.sort()
-    return list_of_tuples
+    print(f'Sorted Inputs: {list_of_tuples}')
 
-try:
-	item_no = int(item_no)
-except Exception as err:
-	print(str(err))
-	sys.exit('Please insert in integer format, i.e. 1 or 5 or 7')
+if __name__ == "__main__":
+    item_no = input('Set the total number of tuples to insert in integer: ')
+    try:
+        item_no = int(item_no)
+    except Exception as err:
+        print(str(err))
+        sys.exit('Please insert in integer format, i.e. 1 or 5 or 7')
 
-items = []
-for i in range(item_no):
-	item = input('Add comma seperated Name(String),Age (Int),Score (Int): ')
-	item = item.split(',')
-	if(len(item)>3):
-		sys.exit('More than 3 items provided')
-	try:
-		item = (str(item[0]),int(item[1]),int(item[2]))
-	except Exception as err:
-		sys.exit(str(err))
-	items.append(item)
+    items = []
+    for i in range(item_no):
+        item = input('Add comma seperated Name(String),Age (Int),Score (Int): ')
+        item = item.split(',')
+        if(len(item)>3):
+            sys.exit('More than 3 items provided')
+        try:
+            item = (str(item[0]),int(item[1]),int(item[2]))
+        except Exception as err:
+            sys.exit(str(err))
+        items.append(item)
 
-print(f'Unsorted Inputs: {items}')
-sorted_items = sorting(items)
-print(f'Sorted Inputs: {sorted_items}')
+    sorting(items)
